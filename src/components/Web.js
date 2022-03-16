03d0315e420326b21f6c2529e6c52db2922c3820
Ulises Armolla <uliarmolla@gmail.com>
2022-03-16 16:57:14 -0500

    Subo ejercitacion terminada

---
+++ src/components/Web.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from './Banner';
+import Encabezado from './Encabezado';
+import Producto from './Producto';
+import Footer from './Footer';
+function Web(){
+  return (
+    <div className="container">
+     <Encabezado />
+     <Banner />
+     <Producto />
+     <Footer />
+    </div>
+  )
+
+}
+export default Web;
